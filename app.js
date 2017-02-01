ddbd08382421db847cf6e09e4a213acd31a08206
Jen Von Essen <cgajen09@gmail.com>
2017-01-31 17:00:33 -0800

    inital directions commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var weather_URL = 'api.openweathermap.org/data/2.5/weather'
+var weather_URL = 'https://api.openweathermap.org/data/2.5/weather';
+var direction_URL = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAtsQkI9rXQT7kf36Giu_qms_Ksowljab4&callback=initMap';
 
 function getWeatherData(callback) {
   var query = {
@@ -6,6 +7,15 @@ function getWeatherData(callback) {
     APPID: 'c463b2d3c8a40702fa35a7648efdae25'
   };
   $.getJSON(weather_URL, query, callback)
+}
+
+function getDirectionData(startCity, endCity, callback) {
+  var query = {
+    origin: startCity,
+    destination: endCity,
+    travelMode: 'DRIVING',
+  }
+  $.getJSON(direction_URL, query, callback)
 }
 
 function displayWeatherData(data) {
@@ -23,6 +33,15 @@ function displayWeatherData(data) {
   // $('.js-search-results').append('<ul class="js-search-results-page-' + page + '">' + result + '</ul>');
 }
 
+function displayDirectionData(data) {
+  var result = '';
+  console.log(data);
+}
+
 $('#weather-button').click(function() {
   getWeatherData(displayWeatherData);
 });
+
+$('#direction-button').click(function() {
+  getDirectionData('Los Angeles, CA', 'Salt Lake City, UT', displayDirectionData)
+})
